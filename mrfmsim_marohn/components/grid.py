9828c8a78a8a7d911bd41804d3f718ab7c3b60e3
Peter Sun <hs859@cornell.edu>
2022-11-03 19:24:51 -0400

    modify function and unit names and parameters

--- mrfmsim_marohn/components/grid.py
+++ mrfmsim_marohn/components/grid.py
@@ -78,10 +78,10 @@ class Grid(ComponentBase):
         ]
 
 
-    def extend_grid_method(self, num_expand):
-        """Extend grid by number of points x, y, z direction"""
+    def extend_grid_method(self, ext_pts):
+        """Extend grid by number of points in x, y, z direction (one side)"""
 
-        ext_shape = self.grid_shape + np.array(num_expand) * 2
+        ext_shape = self.grid_shape + np.array(ext_pts) * 2
         ext_range = (ext_shape - [1, 1, 1]) * self.grid_step
         extents = self.grid_extents(ext_range, self.grid_origin)
 
