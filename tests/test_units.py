b8422e94dbb2734a75ef54e386c175bb4b607989
Peter Sun <hs859@cornell.edu>
2023-10-18 22:27:33 +0800

    Remove units module

--- tests/test_units.py
+++
@@ -1,9 +0,0 @@
-from mrfmsim_marohn import UNITS
-
-def test_units():
-    """Test if units are loaded correctly."""
-    b1 = UNITS["B1"]
-    assert b1["unit"] == "[mT]"
-    assert b1["description"] == "transverse magnetic field"
-    assert b1["latex_label"] == r"$B_1 \: [\mathrm{mT},]$"
-    assert b1['format'] == ":.3f"
