fd14f9aca8b084ed2eac05bf1084e2a42f696b67
Peter Sun <hs859@cornell.edu>
2024-02-01 21:05:56 -0500

    Add custom directive for collection and experiment

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+
+sys.path.insert(0, os.path.abspath("."))
 sys.path.insert(0, os.path.abspath("../../mrfmsim_marohn"))
 sys.path.insert(0, os.path.abspath("../../mrfmsim_marohn/experiment"))
 sys.path.insert(0, os.path.abspath(".."))
@@ -19,10 +20,10 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'MrfmSim-Marohn'
-copyright = '2023, Peter Sun, John A. Marohn'
-author = 'Peter Sun, John A. Marohn, Corinne Isaac'
-release = '0.2.0'
+project = "mrfmSim-marohn"
+copyright = "2023 - 2024, Peter Sun, John A. Marohn"
+author = "Peter Sun, John A. Marohn, Corinne Isaac, and others"
+release = "0.2.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -35,15 +36,69 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx.ext.autosummary",
     "sphinx.ext.intersphinx",
-    'nbsphinx',
+    "nbsphinx",
 ]
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_book_theme'
+html_theme = "sphinx_book_theme"
 html_static_path = []
+
+# -- custom directive for rst -----------------------------------------------------
+
+from docutils.parsers.rst import Directive
+from docutils import nodes
+import importlib
+
+
+class CollectionDirective(Directive):
+    """Discover all experiments in a collection and output their string representation."""
+
+    has_content = True
+    required_arguments = 1
+    optional_arguments = 0
+
+    def run(self):
+
+        name_list = self.arguments[0].split(".")
+        module = importlib.import_module(".".join(name_list[:-1]))
+        module = getattr(module, name_list[-1])
+        node_out = []
+        for expt in module.experiments:
+            child_node = nodes.literal_block(text=str(module[expt]))
+            # create a indentation
+            list_node = nodes.bullet_list(
+                "", nodes.line(text=f"{name_list[-1]}['{expt}']"), child_node
+            )
+
+            node_out.append(list_node)
+
+        return node_out
+
+
+class ExperimentDirective(Directive):
+    """Output experiment string representation."""
+
+    has_content = True
+    required_arguments = 1
+    optional_arguments = 0
+
+    def run(self):
+
+        name_list = self.arguments[0].split(".")
+        module = importlib.import_module(".".join(name_list[:-1]))
+        module = getattr(module, name_list[-1])
+
+        # create a indentation
+        child_node = nodes.literal_block(text=str(module))
+        list_node = nodes.bullet_list("", child_node)
+
+        return [list_node]
+
+
+def setup(app):
+    app.add_directive("collection", CollectionDirective)
+    app.add_directive("experiment", ExperimentDirective)
